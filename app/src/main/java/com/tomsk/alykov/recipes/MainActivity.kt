6b477ac2b84110346f7c4033475fbb650edf99ab
Alykov <aalykov@gmail.com>
2022-11-25 15:19:30 +0700

    Установка Navigation Component

--- app/src/main/java/com/tomsk/alykov/recipes/MainActivity.kt
+++ app/src/main/java/com/tomsk/alykov/recipes/MainActivity.kt
@@ -2,10 +2,28 @@ package com.tomsk.alykov.recipes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.fragmentContainerView)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.recipesFragment, R.id.favoriteRecipiesFragment, R.id.foodJokeFragment))
+        //setupActionBarWithNavController(findNavController(R.id.fragmentContainerView))
+        bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragmentContainerView)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+
 }
