77e7b1b5a0f82b6e3eb3c246c707b03d40d3bf93
Alykov <aalykov@gmail.com>
2022-11-29 19:26:18 +0700

    RecipesFragment Connect the Dots

--- app/src/main/java/com/tomsk/alykov/recipes/ui/MainActivity.kt
+++ app/src/main/java/com/tomsk/alykov/recipes/ui/MainActivity.kt
@@ -7,8 +7,10 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.tomsk.alykov.recipes.R
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/tomsk/alykov/recipes/ui/fragmenst/RecipesFragment.kt
+++ app/src/main/java/com/tomsk/alykov/recipes/ui/fragmenst/RecipesFragment.kt
@@ -1,20 +1,73 @@
 package com.tomsk.alykov.recipes.ui.fragmenst
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.tomsk.alykov.recipes.MainViewModel
 import com.tomsk.alykov.recipes.R
+import com.tomsk.alykov.recipes.adapters.RecipesAdapter
+import com.tomsk.alykov.recipes.util.Constants.Companion.API_KEY
+import com.tomsk.alykov.recipes.util.NetworkResult
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
+@AndroidEntryPoint
 class RecipesFragment : Fragment() {
+    private lateinit var mainViewModel: MainViewModel
+    private lateinit var mView: View
+    private val mAdapter by lazy { RecipesAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        mView = inflater.inflate(R.layout.fragment_recipes, container, false)
+        mainViewModel = ViewModelProvider(requireActivity())[MainViewModel::class.java]
+        setupRecyclerView()
+        requestApiData()
+        return mView
+    }
+
+    private fun setupRecyclerView() {
+        mView.rv_recipes.adapter = mAdapter
+        mView.rv_recipes.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun requestApiData() {
+        mainViewModel.getRecipes(applyQueries())
+        mainViewModel.recipesResponse.observe(viewLifecycleOwner) { response ->
+            when (response) {
+                is NetworkResult.Success -> {
+                    Log.d("AADebug", "requestApiData: NetworkResult.Success")
+                    response.data?.let { mAdapter.setData(it) }
+                }
+                is NetworkResult.Error -> {
+                    Toast.makeText(requireContext(), response.message.toString(), Toast.LENGTH_LONG).show()
+                    Log.d("AADebug", "requestApiData: NetworkResult.Error")
+                }
+                is NetworkResult.Loading -> {
+                    Log.d("AADebug", "requestApiData: NetworkResult.Loading")
+                }
+            }
+        }
+    }
+
+    private fun applyQueries(): HashMap<String, String> {
+        val queries: HashMap<String, String> = HashMap()
+        queries["number"] = "10"
+        queries["apiKey"] = API_KEY
+        queries["type"] = "snack"
+        queries["diet"] = "vegan"
+        queries["addRecipeInformation"] = "true"
+        queries["fillIngredients"] = "true"
+        return queries
     }
 
 }
